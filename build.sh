e90c6ee428dbe7b2a3d8afa924ba5976bfcd6d92
Huy Nguyen Huu <huuhuy@gmail.com>
2017-08-22 13:54:51 +0700

    build cat version, it will enable JSON Service Registry, see DockerFile for more detail

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ cas_version=$1
 if [ $# -eq 0 ] || [ -z "$cas_version" ]
   then
     echo "No CAS version is specified."
-    read -p "Provide a tag for the CAS image you are about to build (i.e. 5.0.0): " cas_version;
+    read -p "Provide a tag for the CAS image you are about to build (i.e. 5.1.3): " cas_version;
 fi
 
 if [ ! -z "$cas_version" ]
   then
   	echo "Build CAS docker image tagged as v$cas_version"
-	docker build --tag="apereo/cas:v$cas_version" . && echo "Built CAS image successfully tagged as v$cas_version" && docker images "apereo/cas:v$cas_version"
+	docker build --tag="huuhuy/cas:v$cas_version" . && echo "Built CAS image successfully tagged as v$cas_version" && docker images "huuhuy/cas:v$cas_version"
   else
   	echo "No image tag is provided."	
 fi
